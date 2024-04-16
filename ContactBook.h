f667b1c557dd341364510e40cc59703e146b18c8
Mudasar Bhatti <134007191+bhatticoder@users.noreply.github.com>
2024-04-16 16:40:01 +0500

    ContactBook.h

--- ContactBook.h
+++ ContactBook.h
@@ -2,9 +2,9 @@
 #include "Contact.h"
 #include <algorithm>
 #include <iomanip>
+#define BASIC_LIB
 #include <iostream>
 #include <string>
-#define BASIC_LIB
 class ContactsBook {
 private:
 	Contact *contacts_list; // array of contacts
@@ -14,20 +14,24 @@ private:
 public:
 	void add_contact(const Contact& contact);
 	int total_contacts() const;
-	Contact* search_contact(string first_name, string last_name);
-	Contact* search_contact(string phone);
+	Contact* search_contact(std::string first_name, std::string last_name);
+	Contact* search_contact(std::string phone);
 	Contact* search_contact(const Address& address);
 	ContactsBook(int size_of_list);
 	~ContactsBook();
-	void print_contacts_sorted(string choice); // Only two choices first_name or last_name
+	ContactsBook(const ContactsBook& other);
+	void print_contacts_sorted(std::string choice); // Only two choices first_name or last_name
 	void merge_duplicates(); // Merge duplicates
 	void print_contacts() const;	// Print all contacts in the order they were added
-	void load_from_file(string file_name);
-	void save_to_file(string file_name);
+	void load_from_file(std::string file_name);
+	void save_to_file(std::string file_name);
 	Contact *copy_list(Contact *contacts_list);
+	// Overload assignment operator
+	ContactsBook& operator=(const ContactsBook& other);
 private:
 	bool full() const;
 	void resize_list();
-	void sort_contacts_list(Contact *contacts_list, string choice);
+	void sort_contacts_list(Contact *contacts_list, std::string choice);
+
 };
 #endif // !BASIC_LIB
